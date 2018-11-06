d09755117de7aff5284849a2a12798ee38a10718
Dishon Jordan <dishonjordan@hotmail.com>
2018-11-06 15:56:39 -0800

    File structure setup, created CMakeLists.txt, and added Parser and ExpressionBuilder to /src

---
+++ src/expressionbuilder.h
@@ -0,0 +1,119 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include "Base.h"
+#include "Operator.h"
+#include "Command.h"
+
+using namespace std;
+
+class ExpressionBuilder {
+private:
+	vector<string> input;
+	vector<int> op_index;
+
+public:
+	ExpressionBuilder(vector<string> commands) {
+
+		input = commands;
+
+		//iterates through command and adds the indices of the operators to op_index
+		for (unsigned int i = 0; i < input.size(); i++) {
+
+			if (input[i].compare(";") == 0 || input[i].compare("&&") == 0 || input[i].compare("||") == 0)
+				op_index.push_back(i);
+
+		}
+
+	}
+
+	//Builds a tree of operators and commands using a vector<string> input.
+	Base* build_tree() {
+		Operator* top = new Operator();
+
+
+		if (input.size() == 0) {//Empty input case
+			return top;
+
+		}
+		else if (op_index.size() == 0) { //No operator case
+
+			Command* c = new Command();
+
+			//Assigning all strings in input to the new command and returning it
+			for (unsigned int i = 0; i < input.size(); i++)
+				c->add(input[i]);
+
+			return c;
+
+
+		}
+		else {
+			//Changing top into a Semicolon/And/Or based off of which one it is
+			if (input[op_index[op_index.size() - 1]].compare(";"))
+				top = new Operator(); //Change to Semicolon
+			if (input[op_index[op_index.size() - 1]].compare("&&"))
+				top = new Operator(); //Change to AND
+			if (input[op_index[op_index.size() - 1]].compare("||"))
+				top = new Operator(); //Change to OR
+
+			//Creating a new command to hold the last command
+			Command* command = new Command();
+
+			//Assigning all strings in input to the new command
+			for (int i = op_index[op_index.size() - 1] + 1; i < input.size(); i++)
+				command->add(input[i]);
+
+			//Assigning that command to the right node of top
+			top->r_node = command;
+
+			//Calling a recusrive helper function to finish the rest of the tree
+			add_children(top, op_index.size() - 2, op_index.size() - 1);
+
+		}
+		return top;
+
+	}
+
+	void add_children(Operator* parent, int left_index, int right_index) {
+
+
+		if (left_index < 0) { //Base case: We are at the first command
+
+			Command* first_command = new Command();
+			for (int i = 0; i < op_index[right_index]; i++)
+				first_command->add(input[i]);
+
+			parent->l_node = first_command;
+
+			return;
+		}
+		Operator* op = new Operator();
+
+		//Changing op into a Semicolon/And/Or based off of which one it is
+		if (input[op_index[right_index]].compare(";"))
+			op = new Operator(); //Change to Semicolon
+		if (input[op_index[right_index]].compare("&&"))
+			op = new Operator(); //Change to AND
+		if (input[op_index[right_index]].compare("||"))
+			op = new Operator(); //Change to OR
+
+		Command* command = new Command();
+
+		//Assigning all strings in input to the new command
+		for (int i = op_index[left_index] + 1; i < op_index[right_index]; i++)
+			command->add(input[i]);
+
+		//Assigning that command to the right node of op
+		op->r_node = command;
+
+		//Assigning the left node of the parent Operator to op
+		parent->l_node = op;
+
+		//Recurse with the left node 
+		add_children(op, left_index - 1, right_index - 1);
+
+	}
+
+};
---
+++ src/parser.h
@@ -0,0 +1,66 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+class Parser {
+
+public:
+	Parser() {}
+
+	vector<string> parse_input(string input) {
+
+		char inputArray[input.size() - 1]; 
+		strcpy(inputArray, input.c_str());
+		vector<string> output;
+
+		char* cptr = strtok(inputArray, " ");
+
+		//Iterating through inputted string and adding each string to the input vector
+		while (cptr != NULL) {
+
+			string temp = cptr;
+			int last_index = temp.length() - 1;
+			size_t comment_index = temp.find("#");
+
+			if (comment_index != string::npos) { //Checking for any comments
+
+				//Making sure we dont add an empty string to the vector
+				if (comment_index > 0)
+					output.push_back(temp.substr(0, comment_index));
+
+				//Everything after the comment is not included, so we break from our parse
+				break;
+
+			}
+			else if (!(strcmp(&temp.back(), ";"))) { // Checking the special semicolon case at the end of the string
+
+				//Making sure we dont add an empty string to the vector
+				if (last_index > 0)
+					output.push_back(temp.substr(0, last_index));
+
+				output.push_back(";");
+
+			}
+			else {
+
+				output.push_back(temp);
+			}
+
+			cptr = strtok(NULL, " ");
+
+		}
+
+
+		return output;
+
+	}
+
+
+
+
+
+};
+
