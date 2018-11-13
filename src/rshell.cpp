e1ea4f832fc48e0d730a15c2380eb726efa2fa66
Dishon Jordan <dishonjordan@hotmail.com>
2018-11-12 19:45:11 -0800

    Added getlogin() and gethostname() functionality printing out client and server info before each $ in rshell.pp

--- src/rshell.cpp
+++ src/rshell.cpp
@@ -2,23 +2,38 @@
 #include <string>
 #include "parser.h"
 #include "expressionbuilder.h"
+#include <stdio.h>
 
 using namespace std;
 
-
 int main() {
+	string input;
+	char user[20];//Defining a char array to hold client username
+	char host[20];//Defining a char array to gold hostname
+	
+	getlogin_r(user,20); //Assigning username
+	if(user == NULL) //Checking for any errors
+	  perror("Could not retrieve username");
+	  
+	gethostname(host,20);//Assigning hostname
+	if(host == NULL) //Checking for any errors
+	  perror("Could not retrieve hostname");
 
-	while (true) {
-		string input;
-		cout << "$ ";
+	while (true) { //Inginite while loop for the main shell procedures
+		
+
+		cout <<"[" << user << "@" << host <<"]$ ";
 		getline(cin, input);
 
 		//quits program if quit is entered
 		if (input.compare("exit") == 0)
 			break;
 
+		//Creating a parser to parse the user input
 		Parser* pat = new Parser();
+		//Creating an ExpressionBuilder to take in the parsed input
 		ExpressionBuilder* bob = new ExpressionBuilder(pat->parse_input(input));
+		//Building the command tree and executing it.
 		bob->build_tree()->execute();
 	}
 
