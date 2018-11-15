8280bf70e53813890654d10d1fc037715f89451f
Travis Nasser <nassertravis@gmail.com>
2018-11-14 16:36:03 -0800

    Added return statement to execute() to prevent a complie time error

--- src/operator.h
+++ src/operator.h
@@ -13,7 +13,7 @@ class Operator : public Base {
         this->l_node = l_node;
 	this->r_node = r_node;
     }
-    bool execute(){}
+    bool execute() { return 0; }
 
     void setLeftNode(Base* left) { l_node = left; }
     void setRightNode(Base* right) { r_node = right; }
