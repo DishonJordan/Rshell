049a26373bda18b5d8ef0b12cb2fd2d5c9b05232
travis nasser <nassertravis@gmail.com>
2018-11-15 22:30:37 -0800

    Fixed segmentation fault bug with comments and trailing operators

--- src/parser.h
+++ src/parser.h
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <string.h>
+#include <string>
 
 using namespace std;
 
@@ -12,21 +14,26 @@ public:
 
 	vector<string> parse_input(string input) {
 
-		char inputArray[input.size() - 1]; 
+		//Look for # and delete everything after it
+		//This code block fixes the bug of seg fault when a operator is followed by a #
+		for (std::string::size_type i = 0; i < input.size(); i++) {
+			if (input.at(i) == '#') {
+				input = input.substr(0, i - 1);
+				break;
+			}
+		}
+
+		//This code block fixes the seg fault special case bug of trailing operator with no right-hand command
+		if (input.at(input.size()-1) == ';')
+			input = input.substr(0, input.size() - 1);
+		if (input.at(input.size()-1) == '&' || input.at(input.size()-1) == '|')
+			input = input.substr(0, input.size() - 2);
+
+		char inputArray[input.size() - 1];
 		strcpy(inputArray, input.c_str());
 		vector<string> output;
 
 		char* cptr = strtok(inputArray, " ");
-
-		//Look for # and delete everything after it
-		int count = 0;
-		for (char& c : input) {
-			if (c == '#') {
-				input.erase(input.at(count), input.size() - 1);
-				break;
-			}
-			count++;
-		}
 
 		//Iterating through inputted string and adding each string to the input vector
 		while (cptr != NULL) {
@@ -34,7 +41,6 @@ public:
 			string temp = cptr;
 			int last_index = temp.length() - 1;
 			size_t comment_index = temp.find("#");
-			
 
 			if (comment_index != string::npos) { //Checking for any comments. meaning a # was found
 
@@ -46,7 +52,7 @@ public:
 				break;
 
 			}
-			else if (!(strcmp(&temp.at(last_index), ";"))) { // Checking the special semicolon case at the end of the string
+			if (!(strcmp(&temp.at(last_index), ";"))) { // Checking the special semicolon case at the end of the string
 
 				//Making sure we dont add an empty string to the vector
 				if (last_index > 0)
