a68c5653124a536f76685391cbf3369c548a4e47
Dishon Jordan <dishonjordan@hotmail.com>
2018-11-21 15:58:49 -0800

    Added further test cases

--- tests/test.cpp
+++ tests/test.cpp
@@ -14,177 +14,414 @@
 
 using namespace std;
 
- /* ExpressionBuilder Testing w/ varying amount of connectors */
- TEST(ExpressionBuilderTEST, EmptyCommandTEST) {
-     vector<string> v;
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-     EXPECT_EQ(0, ep->build_tree()->execute());
+void printParse(string i){
+	 
+	Parser* pat = new Parser();	
+	vector<string> v = pat->parse_input(i);
+	
+	for(int i =0; i < v.size(); i++)
+		cout<< "[" << v[i] << "]";
+	 
+	 
  }
-
- TEST(ExpressionBuilderTEST, NoConnectorTEST) {
-     vector<string> v = {"ls", "-a"};
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-     EXPECT_TRUE(ep->build_tree()->execute());
- }
-
- TEST(ExpressionBuilderTEST, SingleConnectorTEST) {
-     vector<string> v = {"ls", "-l", ";", "echo","hello"};
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-     EXPECT_TRUE(ep->build_tree()->execute());
- }
-
-TEST(ExpressionBuilderTEST, TwoConnectorTEST) {
-     vector<string> v = {"echo", "hello", "||", "ls", "-lA", ";", "ls"};
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-     EXPECT_TRUE(ep->build_tree()->execute());
- }
-
- TEST(ExpressionBuilderTEST, ThreeConnectorTest) {
-     vector<string> v = {"ls", "-a", "&&", "echo", "Hello World", "||", "ls", "-l", ";", "echo", "world"};	
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-     EXPECT_TRUE(ep->build_tree());
- }
-
- TEST(ExpressionBuilderTEST, TopTreeTEST) {
-     vector<string> v = {"echo", "hello", ";", "ls", "-a", "||", "mkdir", "test", "&&", "echo", "world", ";", "git", "status"};
-     ExpressionBuilder* ep = new ExpressionBuilder(v);
-     Base* top = ep->build_tree();
  
-     EXPECT_TRUE(top->execute());
+ void executeExpressionBuilder(string i){
+	 
+	 Parser* pat = new Parser();
+	 ExpressionBuilder* bob = new ExpressionBuilder (pat->parse_input(i));
+	 bob->build_tree()->execute();
+
  }
-
  
-/* Operator Testing */
-TEST(OperatorTEST, AndTEST) {
-    vector<string> v = {"echo", "hello", ";", "echo", "world"};
-    ExpressionBuilder* ep = new ExpressionBuilder(v);
-    
-    EXPECT_TRUE(ep->build_tree()->execute());
-}
 
-TEST(OperatorTEST, OrTEST) {
-    vector<string> v = {"echo", "hello", "||", "echo", "world"};
-    ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-    EXPECT_TRUE(ep->build_tree()->execute());
-}
-
-TEST(OperatorTEST, SemicolonTEST) {
-    vector<string> v = {"echo", "hello", "&&", "echo", "world"};
-    ExpressionBuilder* ep = new ExpressionBuilder(v);
-
-    EXPECT_TRUE(ep->build_tree()->execute());
-}
- 
-/*Parser Testing */
- void printParser(vector<string> v){
-
-  for(int i = 0; i < v.size(); i++)
-    cout << v[i] << " ";
-
-}
-
-TEST(ParerTest, SingleCommand) {
-    string s = "echo hello";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ", output);
-}
-
-TEST(ParerTest, TwoOperators) {
-                string s = "echo hello; echo world || echo hi";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ; echo world || echo hi ", output);
-}
-
-TEST(ParerTest, ManyOperators) {
-                string s = "echo hello; echo world || echo hi; echo hello && echo world";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ; echo world || echo hi ; echo hello && echo world ", output);
-}
-TEST(ParerTest, EmptyInput) {
-                string s = "";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("", output);
-}
-TEST(ParerTest, CommentAfterOperator) {
-                string s = "echo hello; #echo world || echo hi; echo hello && echo world";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ; ", output);
-}
-TEST(ParerTest, CommentWithSpaces) {
-                string s = "echo hello; # echo world || echo hi; echo hello && echo world";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ; ", output);
-}
-TEST(ParerTest, CommentBetweenArgs) {
-
-    string s = "echo hello; echo#world || echo hi; echo hello && echo world";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-    printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("echo hello ; echo ", output);
-}
-TEST(ParerTest, OnlyComment) {
-                string s = "#";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("", output);
-}
-TEST(ParerTest, OnlySemicolon) {
-        string s = ";";
-    Parser* pat = new Parser();
-
-    testing::internal::CaptureStdout();
-                printParser(pat->parse_input(s));
-    string output = testing::internal::GetCapturedStdout();
-
-    EXPECT_EQ("; ", output);
-}
-
- int main(int argc, char** argv) {
-     ::testing::InitGoogleTest(&argc, argv);
-
-     return RUN_ALL_TESTS();
+ TEST(ParserTest, Test1) {
+	
+	testing::internal::CaptureStdout();
+	printParse("");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
  }
+TEST(ParserTest, Test2) {
+	
+	testing::internal::CaptureStdout();
+	printParse(";");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[;]", output);
+ }
+TEST(ParserTest, Test3) {
+	
+	testing::internal::CaptureStdout();
+	printParse("||");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[||]", output);
+ }
+TEST(ParserTest, Test4) {
+	
+	testing::internal::CaptureStdout();
+	printParse("&&");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[&&]", output);
+ }
+TEST(ParserTest, Test5) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A]", output);
+ }
+TEST(ParserTest, Test6) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A;");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;]", output);
+ }
+TEST(ParserTest, Test7) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;][echo][B]", output);
+ }
+TEST(ParserTest, Test8) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A || echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][||][echo][B]", output);
+ }
+TEST(ParserTest, Test9) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A && echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][&&][echo][B]", output);
+ }
+TEST(ParserTest, Test10) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;][echo][B][;][echo][C]", output);
+ }
+TEST(ParserTest, Test11) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;][echo][B][||][echo][C]", output);
+ }
+TEST(ParserTest, Test12) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;][echo][B][&&][echo][C]", output);
+ }
+TEST(ParserTest, Test13) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A || echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][||][echo][B][||][echo][C]", output);
+ }
+TEST(ParserTest, Test14) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A || echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][||][echo][B][;][echo][C]", output);
+ }
+TEST(ParserTest, Test15) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A || echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][||][echo][B][&&][echo][C]", output);
+ }
+TEST(ParserTest, Test16) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A && echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][&&][echo][B][&&][echo][C]", output);
+ }
+TEST(ParserTest, Test17) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A && echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][&&][echo][B][;][echo][C]", output);
+ }
+TEST(ParserTest, Test18) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A && echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][&&][echo][B][||][echo][C]", output);
+ }
+TEST(ParserTest, Test19) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A && echo B || echo C && echo D && echo E || echo F");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][&&][echo][B][||][echo][C][&&][echo][D][&&][echo][E][||][echo][F]", output);
+ }
+TEST(ParserTest, Test20) {
+	
+	testing::internal::CaptureStdout();
+	printParse("#");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ParserTest, Test21) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A #; echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A]", output);
+ }
+TEST(ParserTest, Test22) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; #echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;]", output);
+ }
+TEST(ParserTest, Test23) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; # echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;]", output);
+ }
+TEST(ParserTest, Test24) {
+	
+	testing::internal::CaptureStdout();
+	printParse("echo A; echo hello#world; echo hi");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("[echo][A][;][echo][hello]", output);
+ }
+ 
+TEST(ExpressionBuilderTest, Test1) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ExpressionBuilderTest, Test2) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder(";");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ExpressionBuilderTest, Test3) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("||");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ExpressionBuilderTest, Test4) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("&&");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ExpressionBuilderTest, Test5) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test6) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A;");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test7) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\n", output);
+ }
+TEST(ExpressionBuilderTest, Test8) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A || echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test9) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A && echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\n", output);
+ }
+TEST(ExpressionBuilderTest, Test10) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test11) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\n", output);
+ }
+TEST(ExpressionBuilderTest, Test12) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test13) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A || echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test14) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A || echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test15) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A || echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test16) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A && echo B && echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test17) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A && echo B; echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\nC\n", output);
+ }
+TEST(ExpressionBuilderTest, Test18) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A && echo B || echo C");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\n", output);
+ }
+TEST(ExpressionBuilderTest, Test19) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A && echo B || echo C && echo D && echo E || echo F");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nB\nD\nE\n", output);
+ }
+TEST(ExpressionBuilderTest, Test20) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("#");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("", output);
+ }
+TEST(ExpressionBuilderTest, Test21) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A #; echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test22) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; #echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test23) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; # echo B");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\n", output);
+ }
+TEST(ExpressionBuilderTest, Test24) {
+	
+	testing::internal::CaptureStdout();
+	executeExpressionBuilder("echo A; echo hello#world; echo hi");
+	string output = testing::internal::GetCapturedStdout();
+	
+     EXPECT_EQ("A\nhello\n", output);
+ }
+ 
+ int main(int argc, char **argv){
+	::testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
