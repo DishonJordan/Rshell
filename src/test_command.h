755e18e6d3314a72f587732100d63d9798d3b7f1
Travis <nassertravis@gmail.com>
2018-11-25 00:37:14 -0800

    Wrote test_function() as a member function of Command that handles the 'test' command using stat()

---
+++ src/test_command.h
@@ -0,0 +1,46 @@
+#include "command.h"
+#include <iostream>
+#include "sys/stat.h"
+
+bool Command::test_command() {
+    // Set up for stat()
+    struct stat buf;
+    char* file_path = cmd.back();
+    int result = stat(file_path, &buf);
+
+
+    // Check for the directory flag
+    if (strcmp(cmd[1], "-d") == 0) {
+        if (S_ISDIR(buf.st_mode)) {
+            std::cout << "(True)" << std::endl;
+        }
+        else {
+            std::cout << "(False)" << std::endl;
+            return 0;
+        }
+    }
+
+    // Check for the regular file flag
+    else if (strcmp(cmd[1], "-f") == 0) {
+        if (S_ISREG(buf.st_mode)) {
+            std::cout << "(True)" << std::endl;
+        }
+        else {
+            std::cout << "(False)" << std::endl;
+            return 0;
+        }
+    }
+
+    // Default to -e flag if not specified
+    else {
+        if (result == 0) {
+            std::cout << "(True)" << std::endl;
+        }
+        else {
+            std::cout << "(False)" << std::endl;
+            return 0;
+        }
+    }
+
+    return 1; // If this point is reached, the test must have passed
+}
