3fedafdb2bf8198259058cd71cd598077d7efde7
Dishon Jordan <dishonjordan@hotmail.com>
2018-12-12 11:01:34 -0800

    Fixed indentation consistency, implemented empty set_redirect_params

--- src/operator.h
+++ src/operator.h
@@ -9,14 +9,16 @@ class Operator : public Base {
     /* Constructors */
     Operator() : Base() {
     	l_node = NULL;
-	r_node = NULL;
+	    r_node = NULL;
     }
     Operator(Base* l_node, Base* r_node) : Base() {
         this->l_node = l_node;
-	this->r_node = r_node;
+	    this->r_node = r_node;
     }
     bool execute() { return 0; }
+    void set_redirect_params(int f, int t){}; //Do Nothing
 
     void setLeftNode(Base* left) { l_node = left; }
     void setRightNode(Base* right) { r_node = right; }
+
 };
