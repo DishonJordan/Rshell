d6b9d56796e5d4b731ec29ec1187f4f40301f8f6
Dishon Jordan <dishonjordan@hotmail.com>
2018-12-13 01:14:01 -0800

    Added set parameters lines for left and right child so that they know which input/output to use

--- src/pipe.h
+++ src/pipe.h
@@ -4,29 +4,36 @@
 #include "sys/types.h"
 #include "sys/stat.h"
 #include "stdlib.h"
+ class Pipe : public Operator {
 
-class Pipe : public Operator {
   public:
     Pipe() : Operator() {}
-    Pipe(Base* l_node. Base* r_node) : Operator() {}
+    Pipe(Base* l_node, Base* r_node) : Operator() {}
 
     bool execute() {
         int fds[2];
+        
+		if (pipe(fds) < 0) {
+	    	perror("pipe");
+	    	return 0;
+		}
 
-        if (pipe(fds) == -1) {
-	    perror("pipe");
-	    return 0;
-	}
+		l_node->set_redirect_params(fds[1],1);
 
-	if (!l_node->execute())
-	    return 0;
+ 		if (!l_node->execute())
+	    	return 0;
 
-	close(fds[1]);
+ 		close(fds[1]);
 
-	if (!r_node->execute())
-	    return 0;
+		r_node->set_redirect_params(fds[0],0);
+ 		if (!r_node->execute())
+	    	return 0;
 
-	close(fds[0]);
-	return 1;
+ 		close(fds[0]);
+		return 1;
     }
+
+	void set_redirect_params(int f, int t){
+		l_node->set_redirect_params(f,t);
+	};
 };
